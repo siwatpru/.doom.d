76e005359376d4757ecee5a2ef2c7c92befdc7ee
siwatpru <siwat.pru@outlook.com>
2022-08-10 02:29:21 +0700

    Initial commit

---
+++ custom.el
@@ -0,0 +1,49 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#1c1d20" "#ea3d54" "#78bd65" "#fedd38" "#4fb3d8" "#b978ab" "#4fb3d8" "#cbccd1"])
+ '(custom-safe-themes
+   '("e6f3a4a582ffb5de0471c9b640a5f0212ccf258a987ba421ae2659f1eaa39b09" "850bb46cc41d8a28669f78b98db04a46053eca663db71a001b40288a9b36796c" "da53441eb1a2a6c50217ee685a850c259e9974a8fa60e899d393040b4b8cc922" "9b54ba84f245a59af31f90bc78ed1240fca2f5a93f667ed54bbf6c6d71f664ac" "a6e620c9decbea9cac46ea47541b31b3e20804a4646ca6da4cce105ee03e8d0e" "1704976a1797342a1b4ea7a75bdbb3be1569f4619134341bd5a4c1cfb16abad4" "8d7b028e7b7843ae00498f68fad28f3c6258eda0650fe7e17bfb017d51d0e2a2" "835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" "4b6b6b0a44a40f3586f0f641c25340718c7c626cbf163a78b5a399fbe0226659" "23c806e34594a583ea5bbf5adf9a964afe4f28b4467d28777bcba0d35aa0872e" "d47f868fd34613bd1fc11721fe055f26fd163426a299d45ce69bef1f109e1e71" "7eea50883f10e5c6ad6f81e153c640b3a288cd8dc1d26e4696f7d40f754cc703" "a9a67b318b7417adbedaab02f05fa679973e9718d9d26075c6235b1f0db703c8" "0d01e1e300fcafa34ba35d5cf0a21b3b23bc4053d388e352ae6a901994597ab1" "7a7b1d475b42c1a0b61f3b1d1225dd249ffa1abb1b7f726aec59ac7ca3bf4dae" default))
+ '(exwm-floating-border-color "#32363b")
+ '(fci-rule-color "#5B6268")
+ '(highlight-tail-colors ((("#252d26" "#252d26") . 0) (("#212c32" "#212c32") . 20)))
+ '(jdee-db-active-breakpoint-face-colors (cons "#1B2229" "#fedd38"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#1B2229" "#78bd65"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#1B2229" "#3f444a"))
+ '(minimap-mode nil t)
+ '(objed-cursor-color "#ea3d54")
+ '(package-selected-packages '(leetcode beacon))
+ '(pdf-view-midnight-colors (cons "#cbccd1" "#1c1d20"))
+ '(rustic-ansi-faces
+   ["#1c1d20" "#ea3d54" "#78bd65" "#fedd38" "#4fb3d8" "#b978ab" "#4fb3d8" "#cbccd1"])
+ '(vc-annotate-background "#1c1d20")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#78bd65")
+    (cons 40 "#a4c756")
+    (cons 60 "#d1d247")
+    (cons 80 "#fedd38")
+    (cons 100 "#f8bc33")
+    (cons 120 "#f39b2e")
+    (cons 140 "#ee7b29")
+    (cons 160 "#dc7a54")
+    (cons 180 "#ca797f")
+    (cons 200 "#b978ab")
+    (cons 220 "#c9648e")
+    (cons 240 "#d95071")
+    (cons 260 "#ea3d54")
+    (cons 280 "#bf3e51")
+    (cons 300 "#94404f")
+    (cons 320 "#69424c")
+    (cons 340 "#5B6268")
+    (cons 360 "#5B6268")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
